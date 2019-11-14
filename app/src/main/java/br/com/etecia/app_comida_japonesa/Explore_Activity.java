caa4fe20c1fc5b9e2a8076719ccfda6a0cab6bc2
mjigormj <shinoque321>
2019-11-13 21:33:47 -0300

    Toolbar

---
+++ app/src/main/java/br/com/etecia/app_comida_japonesa/Explore_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.app_comida_japonesa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Explore_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.explore_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/app_comida_japonesa/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.app_comida_japonesa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
